46396ab7df5d768dcd8545bdcc4077c0ce744f1e
Max Strübing <mxstrbng@gmail.com>
2019-12-03 10:50:39 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
+import {render} from 'react-dom';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Slider from './Slider';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const slides = [
+  <div key={0} style={{width: '100%', height: '100%'}}>
+    <img
+      alt="slide1"
+      src="https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    />
+    <p>HELLO</p>
+  </div>,
+  <img
+    key={1}
+    style={{width: '100%', height: '100%'}}
+    alt="slide2"
+    src="https://images.pexels.com/photos/296878/pexels-photo-296878.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+  />,
+  <img
+    key={2}
+    style={{width: '100%', height: '100%'}}
+    alt="slide3"
+    src="https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+  />,
+  <img
+    key={3}
+    style={{width: '100%', height: '100%'}}
+    alt="slide4"
+    src="https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+  />,
+  <img
+    key={4}
+    style={{width: '100%', height: '100%'}}
+    alt="slide4"
+    src="https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+  />,
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return <Slider bullets>{slides}</Slider>;
+};
+
+render(<App />, document.getElementById('root'));
