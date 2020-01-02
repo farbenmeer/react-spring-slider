d2eeae977af9c998652cd940e449ab4b3a077d7d
Max Strübing <mxstrbng@gmail.com>
2020-01-02 10:47:27 +0100

    docs: Use correct output dir for storybook

---
+++ docs/main.4e4c865dd37926550c93.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(461),c(589),n.exports=c(964)},1:function(n,o){},589:function(n,o,c){"use strict";c.r(o);c(590),c(961)}},[[0,1,2]]]);
