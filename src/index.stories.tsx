dc04a544291cae92f537080e7351cc13e56fdf79
Muhammad Rafi <muhraff@gmail.com>
2020-07-19 10:42:43 +1000

    Fixed broken image in slider example

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -6,8 +6,8 @@ import Slider from ".";
 import { ArrowComponentType } from "./components/arrow/arrow";
 
 const images = [
-	"https://images.pexels.com/photos/296878/pexels-photo-296878.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-	"https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	"https://images.pexels.com/photos/3740695/pexels-photo-3740695.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+	"https://images.pexels.com/photos/3740446/pexels-photo-3740446.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
 	"https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 	"https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
 ];
